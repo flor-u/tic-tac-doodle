36991ae35b0f032dfde11541613aa873c845366b
Flor Urbinati <flor29600@hotmail.com>
2019-12-10 22:15:30 +0100

    added server dependencies

--- server/app.js
+++ server/app.js
@@ -4,7 +4,6 @@ const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
 const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
@@ -12,27 +11,37 @@ const path         = require('path');
 const session    = require("express-session");
 const MongoStore = require('connect-mongo')(session);
 const flash      = require("connect-flash");
-    
+const passport = require('passport')
+const cors = require('cors');   
 
-mongoose
-  .connect('mongodb://localhost/server', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+require('./configs/db.config');
+require('./configs/passport/index');
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
 
+
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true
+}
+
+app.use(cors(corsOptions));
 
 // Express View engine setup
 
@@ -43,43 +52,50 @@ app.use(require('node-sass-middleware')({
 }));
       
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
-hbs.registerHelper('ifUndefined', (value, options) => {
-  if (arguments.length < 2)
-      throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
-  if (typeof value !== undefined ) {
-      return options.inverse(this);
-  } else {
-      return options.fn(this);
-  }
-});
+// hbs.registerHelper('ifUndefined', (value, options) => {
+//   if (arguments.length < 2)
+//       throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
+//   if (typeof value !== undefined ) {
+//       return options.inverse(this);
+//   } else {
+//       return options.fn(this);
+//   }
+// });
   
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+// app.locals.title = 'Express - Generated with IronGenerator';
 
 
 // Enable authentication using session + passport
 app.use(session({
-  secret: 'irongenerator',
+  secret: 'draw-now-app',
   resave: true,
   saveUninitialized: true,
-  store: new MongoStore( { mongooseConnection: mongoose.connection })
+  cookie: {
+    secure: false,
+    httpOnly: true,
+    maxAge: 60 * 60 * 24 * 1000
+  },
+  store: new MongoStore( { 
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 })
 }))
 app.use(flash());
 require('./passport')(app);
+
+app.use(passport.initialize())
+app.use(passport.session())
     
 
 const index = require('./routes/index');
 app.use('/', index);
-
-const authRoutes = require('./routes/auth');
-app.use('/auth', authRoutes);
       
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router  = express.Router();
 
+router.use('/api', require('./api'));
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
