512eb5df7b369aa749bb6639a58fdcf86ef269d1
Flor Urbinati <flor29600@hotmail.com>
2019-12-17 10:41:11 +0100

    error2

--- server/app.js
+++ server/app.js
@@ -59,7 +59,7 @@ app.use(require('node-sass-middleware')({
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.png')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 
 
 // hbs.registerHelper('ifUndefined', (value, options) => {
