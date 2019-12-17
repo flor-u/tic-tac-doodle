4d8061d96694ed8d47cd2e51f5441cda5e71362e
Flor Urbinati <flor29600@hotmail.com>
2019-12-17 11:19:21 +0100

    error4

--- server/bin/seeds.js
+++ server/bin/seeds.js
@@ -2,6 +2,7 @@
 
 // To execute this seed, run from the root of the project
 // $ node bin/seeds.js
+require('dotenv').config();
 
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
@@ -10,7 +11,7 @@ const User = require("../models/User");
 const bcryptSalt = 10;
 
 mongoose
-  .connect('mongodb://localhost/server', {useNewUrlParser: true})
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
